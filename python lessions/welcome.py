7ab08fdf24fe4b43e54d101e17f19f655455312e
RAVI NARASIMHA <118276282+RaviNarasimha41@users.noreply.github.com>
2025-09-02 11:58:08 +0530

    Add files via upload

---
+++ python lessions/welcome.py
@@ -0,0 +1,12 @@
+line01="****************"
+line02="*              *"
+line03="*              *"
+line04="*   Welcome!   *"
+line08="*******"
+print()
+print(line01)
+print(line02)
+print(line03)
+print(line04)
+print(line02)
+print(line01)
